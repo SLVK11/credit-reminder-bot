343c001d10e1c35fe26eef8742dfa512a1d71300
SLVK11 <49495840+SLVK11@users.noreply.github.com>
2025-04-24 13:37:01 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,12 +7,12 @@ from datetime import datetime, timedelta
 TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Напиши команду /дата в формате дд.мм.гггг, чтобы узнать крайний срок возврата.")
+    await update.message.reply_text("Привет! Напиши команду /date в формате дд.мм.гггг, чтобы узнать крайний срок возврата.")
 
 async def date_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         if len(context.args) != 1:
-            raise ValueError("Пример: /дата 23.04.2025")
+            raise ValueError("Пример: /date 23.04.2025")
         date_str = context.args[0]
         operation_date = datetime.strptime(date_str, "%d.%m.%Y")
         statement_end = operation_date + timedelta(days=30)
@@ -30,5 +30,5 @@ async def date_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("дата", date_handler))
+    app.add_handler(CommandHandler("date", date_handler))
     app.run_polling()
